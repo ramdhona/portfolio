b7124f19a64a3cf4fbd2ce990269af9dca6f0623
Dwi Ramdona <ramdhona13@gmail.com>
2024-03-30 17:17:49 +0700

    update portofolio + direct form contact ke whatsapp

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,59 +1,59 @@
 //navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector ('header');
-  const fixedNav =  header.offsetTop;
-  const toTop = document.querySelector ('#to-top');
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-  if(window.pageYOffset > fixedNav){
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
-
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
-    header.classList.remove('navbar-fixed');
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
-}
+};
 
 //hamburger
-const hamburger = document.querySelector ('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function () {
-  hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // klik diluar hamburger
-window.addEventListener('click', function (e) {
-  if(e.target != hamburger && e.target != navMenu) {
-    hamburger.classList.remove('hamburger-active');
-     navMenu.classList.add('hidden');
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
   }
 });
 
 //Darkmode toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function () {
+darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
-    html.classList.add('dark');
-    localStorage.theme = 'dark';
-
+    html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
-    html.classList.remove('dark');  
-    localStorage.theme = 'light';
-
+    html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
 
 //pindahkan posisi toggle sesuain mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
   darkToggle.checked = true;
 } else {
   darkToggle.checked = false;
-
 }
-
+////////////
