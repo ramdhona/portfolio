e9ccf1a3e9b29bea52e9df2f8844a797fca5eedd
ramdhona <ramdhona13@gmail.com>
2025-08-27 09:54:33 +0700

    update portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: [
+    "./*.{html,php,js}",
+    "./**/*.{html,php,js}",        // subfolder (components, partials)
+    // kalau pakai Blade/Laravel:
+    // "./resources/views/**/*.blade.php",
+    // kalau pakai React/Vue:
+    // "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
   darkMode: 'class',
   theme: {
-    container :{
-      center:true,
-      padding:'16px',
-    },
+    container: { center: true, padding: '16px' },
     extend: {
-      colors : {
-        primary : '#14b8a6',
-        secondary : '#64748b',
-        dark : '#0F172a'
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0F172a',
       },
-      screens :{
-        '2xl':'1320px'
-      },
+      screens: { '2xl': '1320px' },
     },
   },
   plugins: [],
-}
-
+};
